fa3424e5c100d64b58bac04a4948654ec75e771a
fatbardheminii <fatbardh.emini@hotmail.com>
2025-05-18 01:04:15 +0200

    Add core app with App.jsx, App.css, main.jsx, and initial CSS

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import CartPage from "./pages/CartPage";
+import { CartProvider } from "./context/CartContext";
+
+function App() {
+  return (
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
+}
+
+export default App;
